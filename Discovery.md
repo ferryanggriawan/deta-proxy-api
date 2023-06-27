7c7310ee1bdd22bdd4be3f477489433d617bc680
ferryanggriawan <ferrya1997@gmail.com>
2023-06-27 17:54:20 +0700

    init

---
+++ Discovery.md
@@ -0,0 +1,6 @@
+---
+app_name: Proxy-0.0.1
+tagline: Proxy Release
+---
+
+
---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class _Params(BaseModel):
+    headers: Optional[dict] = None
+    methods: str = "get"
+    url: str
+    params: Optional[dict] = None
+
+
+@app.get("/")
+def root():
+    return {"App": "Proxy API"}
+
+
+@app.post("/proxy-api/json")
+def proxyAPI(item: _Params):
+    methods = item.methods
+    url = item.url
+    params = item.params
+    headers = item.headers
+
+    response = requests.request(method=methods, url=url, headers=headers, params=params)
+
+    return response.json()
